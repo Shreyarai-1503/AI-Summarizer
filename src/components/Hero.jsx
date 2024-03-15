a8d5ac178dabc07abd6d971edacd3e6eb47873e2
Shreya <shreyarai1503@gmail.com>
2024-03-15 12:07:24 +0530

    Project Files

---
+++ src/components/Hero.jsx
@@ -0,0 +1,30 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => window.open("#")}
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Revolutionize content consumption with our AI Article Summarizer.
+        Transform lengthy articles into quick, insightful summaries - the future
+        of efficient information absorption.
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
